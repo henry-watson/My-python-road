55394920c854edf595fe0299f785fc7359dbfc47
henry-watson <371337601@qq.com>
2020-01-16 01:54:54 +0800

    归档根目录下文件到basic文件夹

--- str_lenth.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2020/1/9 19:01
-# @Author  : Henry_Yu
-# @Email   : 371337601@qq.com
-# @File    : str_lenth.py
-# @Software: PyCharm
-
-str = input("请输入字符串，将会告诉你字符串的长度：")
-print(len(str))
